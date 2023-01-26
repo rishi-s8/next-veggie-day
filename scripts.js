7eea47c67c3ed4e67f20663f102207b2e13ca9e1
hkhodr <31921730+hkhodr@users.noreply.github.com>
2023-01-26 11:13:45 +0100

    update anchor

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const possibledays = ["Monday","Tuesday","Thursday"]
 
-const veggyday= new Date('2022-06-07');
+const veggyday= new Date('2023-01-09');
 veggyday.setHours(0,0,0);
 const today = new Date();
 today.setHours(0,0,0);
@@ -13,9 +13,9 @@ var days_difference = Math.round(time_difference / (1000 * 60 * 60 * 24));
 if(!possibledays.includes(day))
     document.getElementById('answer').innerHTML = "NO";
 else{
-    if((day === "Thursday" && (days_difference-9)%21 === 0 ) 
-    || (day === "Monday" && (days_difference-13)%21 === 0 )
-    || (day === "Tuesday" && days_difference%21=== 0 ))
+    if((day === "Thursday" && (days_difference-17)%21 === 0 ) 
+    || (day === "Monday" && (days_difference-)%21 === 0 )
+    || (day === "Tuesday" && (days_difference-8)%21=== 0 ))
         document.getElementById('answer').innerHTML = "YES";
     else
         document.getElementById('answer').innerHTML = "NO"
